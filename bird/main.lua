5a5a1c0276c9d5047dab3fcd200b53e507aaa47e
Matheus Vellosa <80607728+mvellosa@users.noreply.github.com>
2022-08-01 17:17:51 -0300

    adding the paralax effect to the map
    current speeds are  mainly used for debugging and thus are prone to changes

--- bird/main.lua
+++ bird/main.lua
@@ -7,27 +7,43 @@ VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+
+
 local ground = love.graphics.newImage('ground.png')
+
+local bgCurrStart = 0
+local groundCurrStart = 0
+
+local bgloopLocation = 413
+local groundloopLocation = VIRTUAL_WIDTH
+
+local bgSpeed = 50
+local groundSpeed = 100
 
 function love.load() 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 end
 
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
 function love.update(dt)
-    
+    bgCurrStart = (bgCurrStart + bgSpeed * dt) % bgloopLocation
+    groundCurrStart = (groundCurrStart + groundSpeed * dt) % groundloopLocation
 end
 
 function love.draw()
     push:start()
 
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(background, -bgCurrStart, 0)
+    love.graphics.draw(ground, -groundCurrStart, VIRTUAL_HEIGHT - 16)
     push:finish()
 end
 
