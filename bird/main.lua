7ab72ec3fa22d0959f2f1d1143740dc76eb5ee5a
Matheus Vellosa <80607728+mvellosa@users.noreply.github.com>
2022-08-01 16:55:34 -0300

    first commit on the bird game
    drawing a simple background and ground as a base for the game

---
+++ bird/main.lua
@@ -0,0 +1,38 @@
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
+
+function love.load() 
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    push:start()
+
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    push:finish()
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
